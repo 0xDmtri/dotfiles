f2a238f74accf4edb378c50cd52e293f35f69c22
0xDmtri <0xDmtri@protonmail.com>
2024-05-20 13:02:03 +0100

    bump

--- nvim/lua/0xDmtri/init.lua
+++ nvim/lua/0xDmtri/init.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
 	-- Git related plugins
 	{
 		"NeogitOrg/neogit",
-		branch = "nightly",
+		branch = "master",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"sindrets/diffview.nvim",
