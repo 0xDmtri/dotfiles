0619bd458ec9770ee91548a48428f953d71ab54d
0xDmtri <0xDmtri@protonmail.com>
2024-05-27 18:50:41 +0200

    bump

--- nvim/lua/0xDmtri/init.lua
+++ nvim/lua/0xDmtri/init.lua
@@ -213,6 +213,7 @@ require("lazy").setup({
 		-- Rust dev env
 		"mrcjkb/rustaceanvim",
 		version = "^4",
+		lazy = false,
 		dependencies = {
 			"neovim/nvim-lspconfig",
 			"nvim-lua/plenary.nvim",
