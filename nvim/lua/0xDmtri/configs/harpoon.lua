5beb92b4bd88c75a61c51372304fd97076718533
Dmtri <0xDmtri@protonmail.com>
2025-03-31 13:55:26 +0200

    refactor harpoon config

--- nvim/lua/0xDmtri/configs/harpoon.lua
+++ nvim/lua/0xDmtri/configs/harpoon.lua
@@ -5,62 +5,48 @@ local harpoon = require("harpoon")
 
 harpoon:setup({})
 
-wk.add({
-	{ "<leader>h", group = "harpoon" },
-	{
-		"<leader>ha",
-		function()
-			harpoon:list():add()
-		end,
-		desc = "[H]arpoon [A]dd buffer",
-		mode = "n",
-	},
-	{
-		"<leader>hm",
-		function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end,
-		desc = "[H]arpoon [M]enu",
-		mode = "n",
-	},
-	{
-		"<leader>h1",
-		function()
-			harpoon:list():select(1)
-		end,
-		desc = "[H]arpoon file [1]",
-		mode = "n",
-	},
-	{
-		"<leader>h2",
-		function()
-			harpoon:list():select(2)
-		end,
-		desc = "[H]arpoon file [2]",
-		mode = "n",
-	},
-	{
-		"<leader>h3",
-		function()
-			harpoon:list():select(3)
-		end,
-		desc = "[H]arpoon file [3]",
-		mode = "n",
-	},
-	{
-		"<leader>h4",
-		function()
-			harpoon:list():select(4)
-		end,
-		desc = "[H]arpoon file [4]",
-		mode = "n",
-	},
-	{
-		"<M-tab>",
-		function()
-			harpoon:list():next({ ui_nav_wrap = true })
-		end,
-		desc = "which_key_ignore",
-		mode = "n",
-	},
+-- Define the base mappings table
+local harpoon_mappings = {
+    { "<leader>h", group = "+Harpoon" },
+    {
+        "<leader>ha",
+        function()
+            harpoon:list():add()
+        end,
+        desc = "Add buffer",
+        mode = "n",
+    },
+    {
+        "<leader>hm",
+        function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Menu",
+        mode = "n",
+    },
+}
+
+-- Generate select mappings for files 1 through 4 using a loop
+for i = 1, 4 do
+    table.insert(harpoon_mappings, {
+        "<leader>h" .. i,
+        function()
+            harpoon:list():select(i)
+        end,
+        desc = "File [" .. i .. "]",
+        mode = "n",
+    })
+end
+
+-- Add the cycle mapping
+table.insert(harpoon_mappings, {
+    "<M-tab>",
+    function()
+        harpoon:list():next({ ui_nav_wrap = true })
+    end,
+    desc = "which_key_ignore",
+    mode = "n",
 })
+
+-- Register all mappings with Which-Key
+wk.add(harpoon_mappings)
