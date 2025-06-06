817dc9272d2a8d03557798c8dede9161a8274c51
Dmtri <0xDmtri@protonmail.com>
2025-06-06 14:19:55 +0200

    use master branch for telescope

--- nvim/lua/0xDmtri/init.lua
+++ nvim/lua/0xDmtri/init.lua
@@ -169,7 +169,7 @@ require("lazy").setup({
     -- Fuzzy Finder (files, lsp, etc)
     {
         "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
+        branch = "master",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-ui-select.nvim",
