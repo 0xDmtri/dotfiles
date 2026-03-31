766ff1c00cf16a82a5f8be8f14a91b3c52187c26
Dmtri <0xDmtri@protonmail.com>
2026-03-31 13:57:28 +0200

    fmt

--- nvim/.config/nvim/lua/0xDmtri/plugins/snacks.lua
+++ nvim/.config/nvim/lua/0xDmtri/plugins/snacks.lua
@@ -13,8 +13,12 @@ return {
     },
     init = function()
         -- Wire up vim.ui overrides early so other plugins see them
-        vim.ui.select = function(...) return require("snacks").picker.select(...) end
-        vim.ui.input = function(...) return require("snacks").input(...) end
+        vim.ui.select = function(...)
+            return require("snacks").picker.select(...)
+        end
+        vim.ui.input = function(...)
+            return require("snacks").input(...)
+        end
     end,
     config = function(_, opts)
         local snacks = require("snacks")
@@ -22,15 +26,51 @@ return {
 
         require("which-key").add({
             { "<leader>s", group = "+Search" },
-            { "<leader>sG", function() snacks.picker.git_files() end, desc = "Git Files" },
-            { "<leader>sf", function() snacks.picker.files() end, desc = "Files" },
-            { "<leader>sh", function() snacks.picker.help() end, desc = "Help Tags" },
-            { "<leader>sw", function() snacks.picker.grep_word() end, desc = "Word" },
-            { "<leader>sg", function() snacks.picker.grep() end, desc = "Grep" },
+            {
+                "<leader>sG",
+                function()
+                    snacks.picker.git_files()
+                end,
+                desc = "Git Files",
+            },
+            {
+                "<leader>sf",
+                function()
+                    snacks.picker.files()
+                end,
+                desc = "Files",
+            },
+            {
+                "<leader>sh",
+                function()
+                    snacks.picker.help()
+                end,
+                desc = "Help Tags",
+            },
+            {
+                "<leader>sw",
+                function()
+                    snacks.picker.grep_word()
+                end,
+                desc = "Word",
+            },
+            {
+                "<leader>sg",
+                function()
+                    snacks.picker.grep()
+                end,
+                desc = "Grep",
+            },
         })
 
-        vim.keymap.set("n", "<leader>?", function() snacks.picker.recent() end, { desc = "[?] Find recently opened files" })
-        vim.keymap.set("n", "<leader><space>", function() snacks.picker.smart() end, { desc = "[ ] Smart find" })
-        vim.keymap.set("n", "<leader>/", function() snacks.picker.lines() end, { desc = "[/] Fuzzily search in current buffer" })
+        vim.keymap.set("n", "<leader>?", function()
+            snacks.picker.recent()
+        end, { desc = "[?] Find recently opened files" })
+        vim.keymap.set("n", "<leader><space>", function()
+            snacks.picker.smart()
+        end, { desc = "[ ] Smart find" })
+        vim.keymap.set("n", "<leader>/", function()
+            snacks.picker.lines()
+        end, { desc = "[/] Fuzzily search in current buffer" })
     end,
 }
