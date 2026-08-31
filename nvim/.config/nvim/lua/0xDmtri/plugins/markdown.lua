a88817906c0f8a7486877eb124a917c5050100b4
Dmtri <0xDmtri@protonmail.com>
2026-08-31 21:05:17 +0200

    fix(nvim): disable markdown rendering in popup buffers

--- nvim/.config/nvim/lua/0xDmtri/plugins/markdown.lua
+++ nvim/.config/nvim/lua/0xDmtri/plugins/markdown.lua
@@ -7,6 +7,12 @@ return {
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons",
         },
-        opts = {},
+        opts = {
+            overrides = {
+                buflisted = {
+                    [false] = { enabled = false },
+                },
+            },
+        },
     },
 }
